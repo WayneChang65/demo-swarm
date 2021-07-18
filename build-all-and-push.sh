673619387501bf5f595e8af6376f6a15412d64a2
Wayne_Mac <wayne65@gmail.com>
2021-07-18 19:04:12 +0800

    fix: Fix arm/v7 32bits crash problem. Take it off

--- build-all-and-push.sh
+++ build-all-and-push.sh
@@ -1,9 +1,9 @@
 # First, change version tag (e.g. 1.0.1,...)
 sudo docker buildx build \
-    --platform linux/amd64,linux/arm64,linux/arm/v7 \
-    -t waynechang65/show-hostname:1.0.1 \
+    --platform linux/amd64,linux/arm64 \
+    -t waynechang65/show-hostname:1.0.2 \
     --push .
 sudo docker buildx build \
-    --platform linux/amd64,linux/arm64,linux/arm/v7 \
+    --platform linux/amd64,linux/arm64 \
     -t waynechang65/show-hostname:latest \
     --push .
