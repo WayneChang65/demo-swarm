787466cc1fee61410fa36c9de54de28bbd62bd25
Wayne_Mac <wayne65@gmail.com>
2021-07-18 18:40:34 +0800

    feat: Add build files

---
+++ build-all-and-push.sh
@@ -0,0 +1,9 @@
+# First, change version tag (e.g. 1.0.1,...)
+sudo docker buildx build \
+    --platform linux/amd64,linux/arm64,linux/arm/v7 \
+    -t waynechang65/show-hostname:1.0.1 \
+    --push .
+sudo docker buildx build \
+    --platform linux/amd64,linux/arm64,linux/arm/v7 \
+    -t waynechang65/show-hostname:latest \
+    --push .
---
+++ build.sh
@@ -0,0 +1,1 @@
+sudo docker build . -t waynechang65/show-hostname:latest
